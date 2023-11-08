6fc5e902253d54024991fc88249fe47ee2ebf9de
victormc13 <viktorsb14@gmail.com>
2023-11-07 18:44:24 -0800

    Add the 8-uppercase.py function

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+
+    for char in str:
+        if (ord('a') <= ord(char) <= ord('z')):
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            result += uppercase_char
+        else:
+            result += char
+    
+    print(f"{result}")
+
