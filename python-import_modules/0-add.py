87a6208a98574b7df9e632814d0c78b21dafb528
victormc13 <viktorsb14@gmail.com>
2023-11-09 18:28:43 -0800

    Checks if the script is executed as main program

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
