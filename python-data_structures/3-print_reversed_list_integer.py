5c72fa223147680d2b237294eb94f7a922096a14
victormc13 <viktorsb14@gmail.com>
2023-11-11 20:54:32 -0800

    Add a print function to print empty list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if(my_list != []):
+    if (my_list != []):
         my_list.reverse()
         for element in my_list:
             print("{:d}".format(element))
+    print(my_list)
