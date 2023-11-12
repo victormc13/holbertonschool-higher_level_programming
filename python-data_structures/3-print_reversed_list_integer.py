e5c3c24b50e4340ccc933001ce7eee04824fe4ef
victormc13 <viktorsb14@gmail.com>
2023-11-11 22:23:03 -0800

    Add theif statement to check not list passed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 def print_reversed_list_integer(my_list=[]):
 
+    if my_list == None:
+        return []
     reversed_list = my_list[::-1]
     for element in reversed_list:
         print("{:d}".format(element))
