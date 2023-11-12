ef915fa5e8c93f49849352dada5d8343cf999400
victormc13 <viktorsb14@gmail.com>
2023-11-11 22:10:46 -0800

    Replace the print format

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     reversed_list = my_list[::-1]
 
     for element in reversed_list:
-        print("{:d}".format(element))
+        print(f"{element}")
