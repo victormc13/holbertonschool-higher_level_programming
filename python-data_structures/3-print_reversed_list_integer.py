83e4338b4ebf4b1fdc3afdcc06f330a8d654a5da
victormc13 <viktorsb14@gmail.com>
2023-11-11 22:24:39 -0800

    Change condition to is None

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    if my_list == None:
+    if my_list is None:
         return []
     reversed_list = my_list[::-1]
     for element in reversed_list:
