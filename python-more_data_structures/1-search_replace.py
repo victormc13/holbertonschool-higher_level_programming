b9ab7f49223db715b27925662dad3918dfd59a9b
victormc13 <viktorsb14@gmail.com>
2023-11-13 13:57:42 -0800

    Fix pycodestyle coding style

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 
 def search_replace(my_list, search, replace):
 
-    new_list = [replace if element == search else element for element in my_list]
+    lista = [replace if element == search else element for element in my_list]
 
-    return new_list
+    return lista
