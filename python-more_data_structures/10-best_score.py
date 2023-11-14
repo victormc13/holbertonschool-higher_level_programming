447ac7ee8a9f593b5816fb8167e79538cae3736f
victormc13 <viktorsb14@gmail.com>
2023-11-14 15:09:40 -0800

    Fix coding style following pycodestyle rules

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,19 +2,18 @@
 
 from functools import reduce
 
+
 def best_score(a_dictionary):
 
     if a_dictionary is None:
-       return None
-
-#    best_key = max(a_dictionary, key=a_dictionary.get) 
+        return None
 
     def best_value(k1, k2):
         if a_dictionary[k1] > a_dictionary[k2]:
             return k1
         else:
             return k2
-    
+
     best_key = reduce(best_value, a_dictionary.keys())
 
     return (best_key)
