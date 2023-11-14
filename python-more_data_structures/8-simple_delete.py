8d504d0039dfbbac8422d7e6d06e669d7fa08888
victormc13 <viktorsb14@gmail.com>
2023-11-13 21:59:47 -0800

    Add an and operator to check that dictionary is not None

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 
 def simple_delete(a_dictionary, key=""):
 
-    if key is a_dictionary:
+    if key is a_dictionary and a_dictionary is !None:
         del a_dictionary[key]
 
     return a_dictionary
