96feb78e068d310cd4aee13d651bdd2255cd20a5
victormc13 <viktorsb14@gmail.com>
2023-11-14 15:13:49 -0800

    Refactor the code without the reduce module

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
 
-from functools import reduce
-
-
 def best_score(a_dictionary):
 
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
 
-    def best_value(k1, k2):
-        if a_dictionary[k1] > a_dictionary[k2]:
-            return k1
-        else:
-            return k2
-
-    best_key = reduce(best_value, a_dictionary.keys())
-
-    return (best_key)
+    return max(a_dictionary, key=a_dictionary.get)
