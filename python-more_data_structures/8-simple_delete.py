b1668677563d4d89d16056f7b072739510c7d8be
victormc13 <viktorsb14@gmail.com>
2023-11-13 22:06:24 -0800

    Return a tuple with a dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -5,4 +5,4 @@ def simple_delete(a_dictionary, key=""):
     if key is a_dictionary and a_dictionary is not None:
         del a_dictionary[key]
 
-    return a_dictionary
+    return (a_dictionary)
