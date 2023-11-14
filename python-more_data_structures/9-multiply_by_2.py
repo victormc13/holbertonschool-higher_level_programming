cc77c574d90a765d91be0e0cff112e832ba83947
victormc13 <viktorsb14@gmail.com>
2023-11-13 22:41:48 -0800

    Fix the coding style following pycodestyle rules

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 def multiply_by_2(a_dictionary):
 
-    new_dictionary = dict(map(lambda kv: (kv[0], kv[1] * 2), a_dictionary.items()))
+    new_dict = dict(map(lambda kv: (kv[0], kv[1] * 2), a_dictionary.items()))
 
-    return (new_dictionary)
+    return (new_dict)
