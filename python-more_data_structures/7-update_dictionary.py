cd9cbcf70e098877a4f890ef07989905b7203841
victormc13 <viktorsb14@gmail.com>
2023-11-13 21:23:48 -0800

    Add return statement to return a dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,7 @@ def update_dictionary(a_dictionary, key, value):
 
     if a_dictionary is None:
         a_dictionary = {}
-        a_dictionary[key] = value
 
     a_dictionary[key] = value
+
+    return (a_dictionary)
