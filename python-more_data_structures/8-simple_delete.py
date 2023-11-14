bb8e68673f542f624fc1dfbe84d52079e053cf77
victormc13 <viktorsb14@gmail.com>
2023-11-13 22:20:13 -0800

    Change is for in to check if a key is in dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 
 def simple_delete(a_dictionary, key=""):
 
-    if key is a_dictionary and a_dictionary is not None:
+    if key in a_dictionary:
         del a_dictionary[key]
 
     return (a_dictionary)
