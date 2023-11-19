d4c687be1f55a4f2b46a0f8aba275ee8aab39c73
victormc13 <viktorsb14@gmail.com>
2023-11-19 15:53:21 -0800

    Add instance attributes instead of private attributes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -85,7 +85,7 @@ class Square:
         if self.size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for _ in range(self.position[1]):
                 print()
             for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                print(" " * self.position[0] + "#" * self.size)
