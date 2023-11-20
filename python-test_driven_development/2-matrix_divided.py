920e51823fd9ce7ab41f0d7ba1a1373d964da8ea
victormc13 <viktorsb14@gmail.com>
2023-11-20 10:35:18 -0800

    Fix pycodestyle coding style

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,8 +20,9 @@ def matrix_divided(matrix, div):
         list: All elements of the matrix divided by div, rounded to 2 decimal.
         """
     if not isinstance(matrix, list) or not all(isinstance(row, list) and all(
-        isinstance(i, (int, float)) for i in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            isinstance(i, (int, float)) for i in row) for row in matrix):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
@@ -29,7 +30,9 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    result_matrix = [
+            [round(element / div, 2) for element in row]
+            for row in matrix
+    ]
 
     return result_matrix
-
