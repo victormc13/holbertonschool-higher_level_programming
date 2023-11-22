2cfc3d564dadf2a41ae2113a240272e65223a2d6
victormc13 <viktorsb14@gmail.com>
2023-11-21 18:11:33 -0800

    Removes commented code

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -94,6 +94,5 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             perimeter = 0
-        # else:
         perimeter = 2 * (self.__width + self.__height)
         return perimeter
