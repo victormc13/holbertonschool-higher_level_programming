58486639553c359803fd829c62114df0e5d89463
victormc13 <viktorsb14@gmail.com>
2023-11-21 18:12:52 -0800

    Use the if/else statement to return the perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -94,5 +94,6 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             perimeter = 0
-        perimeter = 2 * (self.__width + self.__height)
+        else:
+            perimeter = 2 * (self.__width + self.__height)
         return perimeter
