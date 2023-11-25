3d379ed97cf38b430bf3f6340b0925a2560256a4
victormc13 <viktorsb14@gmail.com>
2023-11-25 14:56:25 -0800

    Use the name argument as output when rises an Exception

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -27,6 +27,6 @@ class BaseGeometry:
             TypeValue: If value is less or equal to 0.
         """
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
