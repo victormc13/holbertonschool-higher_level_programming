2fb04a281a44f67d4d0c257a16ada6bf9baafa79
victormc13 <viktorsb14@gmail.com>
2023-11-25 07:47:43 -0800

    Refactor the code using the type function to check the type of the object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -13,4 +13,4 @@ def is_same_class(obj, a_class):
     Returns:
         bool: True if object is an instance of a_class, otherwise returns False.
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
