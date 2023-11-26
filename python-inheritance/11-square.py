9ef51e7a2b1f680d95ffebdcafd646f92e69b1fc
victormc13 <viktorsb14@gmail.com>
2023-11-25 21:21:21 -0800

    Adds the magic method str to print a representation of the instance

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Call base Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+"""Square class"""
+
+
+class Square(Rectangle):
+    """Defines a square that inherits from Rectangle class."""
+    def __init__(self, size):
+        """
+        Initialize the square class
+        validating size value.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return a informal string representation of the instance.
+
+        Returns:
+            str: String representatino of the instance
+            using size attribute.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
