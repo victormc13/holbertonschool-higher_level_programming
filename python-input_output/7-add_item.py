f2f8f87a87c000b27ec90576852b7c5a5304eb09
victormc13 <viktorsb14@gmail.com>
2023-11-26 23:14:29 -0800

    Add docstring documentation for the module 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""A script that adds all arguments to a Python list, and then save them to a file."""
+
+
 """Imported modules"""
 from sys import argv
 from os.path import exists
