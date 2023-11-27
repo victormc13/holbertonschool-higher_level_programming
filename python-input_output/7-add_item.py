ca34b0475c2e25f0c15cec9d2198600a0ded4a2c
victormc13 <viktorsb14@gmail.com>
2023-11-26 23:01:21 -0800

    Fix typo error in the filename variable

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,15 +5,17 @@ from os.path import exists
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filenama = "add_item.json"
+filename = "add_item.json"
 
 # Check if the file exists, load its content if it does
 if exists(filename):
     py_list = load_from_json_file(filename)
 else:
-    # Add command line arguments to the list
     py_list = []
-    py_list.extend(argv[1:])
+
+
+# Add command line arguments to the list
+py_list.extend(argv[1:])
 
 # Save the updated list to the file
 save_to_json_file(py_list, filename)
