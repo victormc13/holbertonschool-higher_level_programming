67e17d4de61e9552c9b8b847e1c387a483562ef4
victormc13 <viktorsb14@gmail.com>
2023-11-26 20:42:46 -0800

    Adds docstring documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,10 @@
 
 
 def read_file(filename=""):
+    """Prints in stdout the content of filenamei.
+    
+    Args:
+        filename (file): File to print it's content.
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read())
