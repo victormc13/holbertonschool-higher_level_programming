9823274099c7bea7f5a3c68e3abd842a2ccbf4f8
victormc13 <viktorsb14@gmail.com>
2023-11-26 22:59:47 -0800

    If the filename doesn't exists create an empty list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,13 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filenama = "add_item.json"
-py_list = []
 
 # Check if the file exists, load its content if it does
 if exists(filename):
     py_list = load_from_json_file(filename)
 else:
     # Add command line arguments to the list
+    py_list = []
     py_list.extend(argv[1:])
 
 # Save the updated list to the file
