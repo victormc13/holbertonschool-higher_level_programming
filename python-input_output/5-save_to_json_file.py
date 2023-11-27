66254b55cb9c3e8f1db15bb66106d6a46118c8f7
victormc13 <viktorsb14@gmail.com>
2023-11-26 22:13:29 -0800

    Use the dumps function instead of dump

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
         filename (file): File to put the parsed object.
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(json.dump(my_obj, filename))
+        f.write(json.dumps(my_obj))
