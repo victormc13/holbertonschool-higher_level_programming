6b4925574b652be2e46f69f6f97722d781253be2
victormc13 <viktorsb14@gmail.com>
2023-11-26 22:10:47 -0800

    Uses the with statement

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -13,4 +13,5 @@ def save_to_json_file(my_obj, filename):
         my_obj (obj): Object to parse to JSON.
         filename (file): File to put the parsed object.
     """
-    json.dump(my_obj, filename)
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, filename)
