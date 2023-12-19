6dae144c1067bcb450d34185b816c505bb24ecd8
victormc13 <viktorsb14@gmail.com>
2023-12-18 21:56:08 -0800

    Remove the * wrong character

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
 -- Removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in the MySQL server.
-DELETE * FROM second_table WHERE socre <= 5;
+DELETE FROM second_table WHERE socre <= 5;
