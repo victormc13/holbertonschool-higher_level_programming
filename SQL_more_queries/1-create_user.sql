3f838ce59366bd72a69219e7b5baa605a6372ec6
victormc13 <viktorsb14@gmail.com>
2023-12-24 01:33:31 -0500

    Adds a script that creates the MySQL server user user_0d_1 and grants all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+/* Creates the MySQL server user user_0d_1 and grants all privileges */
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; -- create user
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION; -- grant privileges
