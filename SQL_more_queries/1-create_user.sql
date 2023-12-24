3d6086fff3e0270e29308c0eeeda2695dacb27ff
victormc13 <viktorsb14@gmail.com>
2023-12-24 14:33:33 -0500

    Moves the comments to the side of the queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 /* creates the MySQL server user user_0d_1 and grants all privileges */
--- create user if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
--- grants all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- update privileges
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';  -- create user if not exists
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;  -- grants all privileges to the user
+FLUSH PRIVILEGES;  -- update privileges
 
