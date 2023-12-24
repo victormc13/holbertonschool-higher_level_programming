ab69257462a243c01716b289fd67ce16f545832b
victormc13 <viktorsb14@gmail.com>
2023-12-24 12:54:08 -0500

    Adds a comment before each query

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- creates the MySQL server user user_0d_1 and grants all privileges
+/* creates the MySQL server user user_0d_1 and grants all privileges */
+-- create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
+-- grants all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- update privileges
 FLUSH PRIVILEGES;
+
