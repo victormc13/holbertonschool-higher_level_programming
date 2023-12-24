b56a7426a025aa6820b8d9b5c33f4ffa24fa61f9
victormc13 <viktorsb14@gmail.com>
2023-12-24 02:14:20 -0500

    Update the privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates the MySQL server user user_0d_1 and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
