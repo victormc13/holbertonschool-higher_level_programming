09971f2f2ab2eee10b9fec5c6e6eb7bfab82fb3c
victormc13 <viktorsb14@gmail.com>
2023-12-26 13:41:36 -0500

    Adds the FLUSH PRIVILEGES clause to update privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creates the MySQL server user user_0d_1 and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';  
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
 
