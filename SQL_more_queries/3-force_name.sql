7f9b62a75f9a3a774e7342fcd4da228cf1a86ec3
victormc13 <viktorsb14@gmail.com>
2023-12-26 11:43:34 -0500

    Create the table if not exists otherwise the script should not fail

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates the table force_name with id and name columns
-CREATE TABLE force_name (id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
