6bd5f15aa304d39c440631a4b921873a392f0075
victormc13 <viktorsb14@gmail.com>
2023-12-26 21:41:06 -0500

    Use INNER JOIN instead of JOIN clause

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT id, name, states.name
 FROM cities
-JOIN states ON state_id = id
+INNER JOIN states ON state_id = id
 ORDER BY id ASC;
 
