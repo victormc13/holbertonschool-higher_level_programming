9f0cd0798a29a373887dc06007e382a801db8b49
victormc13 <viktorsb14@gmail.com>
2023-12-26 21:43:08 -0500

    Uses the states.id from the states table for the JOIN clause

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT id, name, states.name
 FROM cities
-INNER JOIN states ON state_id = id
+JOIN states ON state_id = states.id
 ORDER BY id ASC;
 
