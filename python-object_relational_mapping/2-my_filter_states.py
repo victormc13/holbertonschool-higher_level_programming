421783a4be4db55a378180ec934ecb5d652abdfa
victormc13 <viktorsb14@gmail.com>
2023-12-30 12:07:05 -0500

    Adds a script that displays all values in the states table of hbtn_0e_0_usa where name matches the user argument.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    # Connect to MySQL server
+    try:
+       db = MySQLdb.connect(
+               host='localhost',
+               port=3306,
+               user=username,
+               passwd=password,
+               db=database
+        )
+    except Exception:
+        print("Could not connect to database")
+        sys.exit(1)
+
+    # Create cursor object
+    cur = db.cursor()
+
+    # Execute query with user input
+    query = "SELECT * FROM states\
+            WHERE name = '{}'\
+            ORDER BY id ASC;".format(state_name)
+    cur.execute(query)
+
+    # Fetch all rows and display results
+    results = cur.fetchall()
+    for row in reaults:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
