6d08f7ddc7023221bf514d068e3db3016a692e49
victormc13 <viktorsb14@gmail.com>
2023-12-29 23:34:50 -0500

    Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+""" lists all states with a name starting with N(upper N)
+    from the database hbtn_0e_0_usa
+"""
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        db = MySQL.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+        )
+    except Exception:
+        print("Could not connect to database")
+        sys.exit(1)
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Execute query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+
+    # Fetch all rows and display results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection}
+    cursor.close()
+    db.close()
