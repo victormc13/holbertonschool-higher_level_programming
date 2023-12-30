7fa8b59798fb71218c1e5d9394964d0b08686c57
victormc13 <viktorsb14@gmail.com>
2023-12-29 21:28:45 -0800

    Refactor documentation module

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-"""
-lists all states with a name starting with N(upper N)
-the database hbtn_0e_0_usa
-"""
+"""lists all states with a name starting with N(upper N)
+the database hbtn_0e_0_usa"""
 
 
 if __name__ == "__main__":
