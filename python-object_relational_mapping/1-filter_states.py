1843cb49cbbb3d8e4ec280790e50271b5e390ed4
victormc13 <viktorsb14@gmail.com>
2023-12-29 21:02:01 -0800

    Fix the correct name MySQLdb module

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     # Connect to MySQL server
     try:
-        db = MySQL.connect(
+        db = MySQLdb.connect(
                 host='localhost',
                 port=3306,
                 user=username,
