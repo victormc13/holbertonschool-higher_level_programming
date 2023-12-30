ee98a84a879552509ca28adf13e4fe3ddc8145f6
victormc13 <viktorsb14@gmail.com>
2023-12-29 22:05:55 -0800

    Modifies the documentation module

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-"""lists all states with a name starting with N(upper N)
-the database hbtn_0e_0_usa"""
+"""
+lists all states with a name starting with N(upper N)
+from the database hbtn_0e_0_usa.
+"""
 
 
 if __name__ == "__main__":
