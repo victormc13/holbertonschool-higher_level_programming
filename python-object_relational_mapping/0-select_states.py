3f2c47f0890701a3f6c88acea35935002f8e6465
victormc13 <viktorsb14@gmail.com>
2023-12-29 19:43:40 -0500

    Removes argument validation and use try/except statement for connection to database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
 
-"""list all states from the database hbtn_0e_0e_usa
-"""
+"""list all states from the database hbtn_0e_0e_usa."""
 
-import MySQLdb
-import sys 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+    import MySQLdb
+    import sys
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-
     # Connect to MySQL server
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-    )
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                port=3306,
+                user=username,
+                passwd=password,
+                db=database
+        )
+    except Exception:
+        print("Could not connect to database")
+        sys.exit(1)
 
     # Create cursor object
     cursor = db.cursor()
