ddb61ea8f4c3db6c9824d87c76e4a5d93633415e
victormc13 <viktorsb14@gmail.com>
2023-12-30 17:37:15 -0800

    Adds a script that lists all cities from the database hbtn_0e_4_usa

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""list all cities from the database hbtn_0e_4_usa"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(
+               host='localhost',
+               port=3306,
+               user=username,
+               passwd=password,
+               db=database
+        )
+    except Exception:
+        print("Could not connect to database")
+        sys.exit(1)
+
+    # Create cursor object
+    cur = db.cursor()
+
+    # Execute query with user input
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC;")
+
+    # Fetch all rows and display results
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
