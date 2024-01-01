b7ac2253509db17a9ca418dccafdffacce935660
victormc13 <viktorsb14@gmail.com>
2023-12-31 17:17:32 -0800

    Fix pycodestyle coding style comment identation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    #Create a MySQL engine
+    # Create a MySQL engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
