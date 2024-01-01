d088e693733ac9c10c0bae00b3e84c25f46e379f
victormc13 <viktorsb14@gmail.com>
2023-12-31 16:48:48 -0800

    Adds the class definition of a State that represents a table

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define the State class and link it to the states table in the database."""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = decalartive_base()
+
+
+class State(Base):
+    """State class that represents the states table."""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
