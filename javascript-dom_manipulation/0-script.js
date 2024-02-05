4a6f939d57aaf59bc3c939799aa4eb3b5239ebe7
victormc13 <viktorsb14@gmail.com>
2024-02-05 08:56:11 -0800

    new: JavaScript script that updates the text color of the header element to red (#FF0000)

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+        const headerElement = document.querySelector("header");
+        headerElement.style.color = "#FF0000"
+});
