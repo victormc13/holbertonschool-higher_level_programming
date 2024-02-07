987562a774b232a91841cbf340668eb50bdd411d
victormc13 <viktorsb14@gmail.com>
2024-02-07 13:36:42 -0500

    udpate: Updates comments for better documentation

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,12 +1,12 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Select the tag with id red_header and header element
+  // create variables for the li element and elements by class and id
   const liElement = '<li>Item</li>'
   const myList = document.querySelector(".my_list")
   const addItem = document.querySelector("#add_item")
-  // Add a click event listener to the red_header tag
+  // Add a click event listener to the add_item id tag
   addItem.addEventListener("click", () => {
-    // Adds the class 'red' to the header element when the user clicks
+    // Adds the liElement to myList when the user clicks on addItem
     myList.insertAdjacentHTML("beforeend", liElement)
   });
 });
