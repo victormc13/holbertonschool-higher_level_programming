6d8f2002b91689b312854e74f214e762a61f5356
victormc13 <viktorsb14@gmail.com>
2024-02-07 13:34:11 -0500

    update: Update script comments for better documentation

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-  // Select the tag with id red_header and header element
+  // Select the tag with id toggle_header and header element
   const header = document.querySelector("header");
   const toggleHeader = document.getElementById("toggle_header");
-  // Add a click event listener to the red_header tag
+  // Add a click event listener to the toggle_header id tag
   toggleHeader.addEventListener("click", () => {
-    // Adds the class 'red' to the header element when the user clicks
+    // Toggles the class 'red' and 'green' to the header element when the user clicks
     greenClass = header.classList.contains("green")
     if (greenClass) {
       header.classList.replace("green", "red")
