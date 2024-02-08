bc29d81478fc816553201bdee95010334ad9bf15
victormc13 <viktorsb14@gmail.com>
2024-02-08 01:31:14 -0500

    new: Script that fetches and prints how to say “Hello” depending on the language selected

---
+++ javascript-dom_manipulation/101-script.js
@@ -0,0 +1,23 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Select HTML elements
+  const languageSelect = document.querySelector("#language_code");
+  const translateBtn = document.querySelector("#btn_translate");
+  const helloElement = document.querySelector("#hello");
+
+  const fetchTranslation = () => {
+    // Get the selected language code
+    const languageCode = languageSelect.value;
+    // URL for the fetching data
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        helloElement.textContent = data.hello;
+      });
+  };
+
+  // Event listener for the translate button
+  translateBtn.addEventListener("click", fetchTranslation)
+});
